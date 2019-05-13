d04a281fa9f60de4ee3e402667500b0b6680cead
MarkDottyD <50585347+MarkDottyD@users.noreply.github.com>
2019-05-13 14:57:28 +0100

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Advertise your business completely free of charge on www.DottyDirectory.com and we’ll also promote your business locally on our national network of over 70 websites and on our social media pages – again completely free of charge. For more information please visit our website.`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
